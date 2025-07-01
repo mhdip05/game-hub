b10b3ec2e510e8b1c0711fafd65c5de9a8d71bdc
MH Dip <mhdip05@outlook.com>
2025-07-01 16:27:54 -0600

    Clean up the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,9 +13,8 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        
-        <HStack justifyContent="space-between">
+      <CardBody> 
+        <HStack justifyContent="space-between" mb={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
