3b7132394e7011530a8ee6797f6bc5109f6e1aca
MH Dip <mhdip05@outlook.com>
2025-06-30 22:11:21 -0600

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -23,6 +24,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   return (
     <>
+     <Heading fontSize='2xl' mb={3}>Genres</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
@@ -30,9 +32,12 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit='cover'
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                whiteSpace='normal'
+                textAlign='left'
                 fontWeight={
                   genre.id === selectedGenre?.id ? "bold" : "normal"
                 }
